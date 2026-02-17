8e8657942b76f3e03f6b3cb92791f3cac85b07c2
Jack <jackrojasduran25@email.com>
2026-02-16 22:15:54 -0500

    portrait.js

--- projects/portrait/portrait.js
+++ projects/portrait/portrait.js
@@ -1,11 +1,17 @@
-const canvas = document.getElementBYId("myCanvas");
+const canvas = document.getElementBYId("myCanvas");i
 const ctx = canvas.getContext("2d");
 
 
 <!-----BACKGROUD----->
 ctx.fillStyle = "cool grey";
-ctx.fillRect(0, 50, 600, 10);
-ctx.fillRect(0, 100, 600, 10);
-ctx.fillRect(0, 150, 600, 10);
-ctx.fillRect(0, 150, 600, 10);
+ctx.fillRect(0, 0, 800, 800);
+
+ctx.fillStyle = "Silver";
+ctx.fillRect(0, 60, 800, 10);
+ctx.fillRect(0, 115, 800, 10);
+ctx.fillRect(0, 170, 800, 10);
+ctx.fillRect(0, 230, 800, 10);
+
+
+
 
