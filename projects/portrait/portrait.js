db5b3712d7770e374031bd81dba24c1de55aa886
Jack <jackrojasduran25@email.com>
2026-02-16 22:20:49 -0500

    portrait

--- projects/portrait/portrait.js
+++ projects/portrait/portrait.js
@@ -1,12 +1,12 @@
-const canvas = document.getElementBYId("myCanvas");
+const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 
 <!-----BACKGROUD----->
-ctx.fillStyle = "cool grey";
+ctx.fillStyle = "d9d9d9";
 ctx.fillRect(0, 0, 800, 800);
 
-ctx.fillStyle = "Silver";
+ctx.fillStyle = "bfbfbf";
 ctx.fillRect(0, 60, 800, 10);
 ctx.fillRect(0, 115, 800, 10);
 ctx.fillRect(0, 170, 800, 10);
