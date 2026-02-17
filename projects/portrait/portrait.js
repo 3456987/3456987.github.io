3462243a83f99bba5c86aead01398714c68cfbf1
Jack <jackrojasduran25@email.com>
2026-02-16 22:01:06 -0500

    portrait

---
+++ projects/portrait/portrait.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementBYId("myCanvas");
+const ctx = canvas.getContext("2d")
+
+
+<!-----BACKGROUD----->
+ctx.fillStyle = "cool grey";
+ctx.fillRect(0, 50, 600, 10);
+ctx.fillRect(0, 100, 600, 10);
+ctx.fillRect(0, 150, 600, 10);
+ctx.fillRect(0, 150, 600, 10);
+
