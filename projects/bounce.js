b7cd0fc242c6271a16f1fd592f3c032108341022
Jack <jackrojasduran25@email.com>
2026-03-17 12:13:28 -0400

    ok

--- projects/bounce.js
+++ projects/bounce.js
@@ -3,9 +3,25 @@ const ctx = canvas.getContext("2d");
 
 let x = 0;
 let y = 0;
+
+let vx = 1;
+let vy = 2;
+
 function animate(){
-	x = x + 1;
-	y = y + 2;
+	//First, clear the screen
+	ctx.clearRect(0,0,800,800);
+
+
+	//decide where the box should be
+	x = x + vx;
+	y = y + vy;
+
+	if (x > 750 || y < 0){
+		vx = vx * -1;
+	}
+	if(y > 750 || y < 0){
+		vy = vy * -1;
+
 	ctx.fillRect(x,y,50,50);
 	requestAnimationFrame(animate);
 }
