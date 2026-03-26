6ffa9941218a71374adefa8701e2bb60f1921282
Jack <jackrojasduran25@email.com>
2026-03-26 12:08:44 -0400

    loop

---
+++ practice/loops/loops.js
@@ -0,0 +1,23 @@
+let canvas = document.getElementById("canvas1");
+let ctx = canvas.getContext("2d");
+
+//fill in code for canvas1 here
+for(let x = 0; x < 800; x += 100){
+	ctx.fillRect(x,0,50,20);
+}
+
+canvas = document.getElementById("canvas2");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas2 here
+
+canvas = document.getElementById("canvas3");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas3 here
+
+canvas = document.getElementById("canvas4");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas4 here
+
