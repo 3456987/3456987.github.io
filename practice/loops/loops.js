0c8f66f39955f74a337a8e308146880153fd1b6a
Jack <jackrojasduran25@email.com>
2026-03-26 12:19:26 -0400

    loops

--- practice/loops/loops.js
+++ practice/loops/loops.js
@@ -2,10 +2,10 @@ let canvas = document.getElementById("canvas1");
 let ctx = canvas.getContext("2d");
 
 //fill in code for canvas1 here
-for(let y = 0; y <= 300; y += 10){
-for(let x = 25; x < 800; x += 100){
-	ctx.fillRect(x,y,50,20);
-}
+for(let y = 0; y <= 800; y += 100){
+	for(let x = 25; x < 800; x += 100){
+		ctx.fillRect(x, y, 50, 20);
+	}
 }
 canvas = document.getElementById("canvas2");
 ctx = canvas.getContext("2d");
