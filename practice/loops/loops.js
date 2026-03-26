bc04b76d9da39f438c83163d32698109e03f0648
Jack <jackrojasduran25@email.com>
2026-03-26 12:45:09 -0400

    lost code

---
+++ practice/loops/loops.js
@@ -0,0 +1,24 @@
+let canvas = document.getElementById("canvas1");
+let ctx = canvas.getContext("2d");
+
+//fill in code for canvas1 here
+for(let y = 0; y <= 800; y += 100){
+	for(let x = 25; x < 800; x +=100){
+		ctx.fillRect(x, y, 50, 20);
+	}
+}
+
+canvas = document.getElementById("canvas2");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas2 here
+
+canvas = document.getElementById("canvas3");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas3 here
+
+canvas = document.getElementById("canvas4");
+ctx = canvas.getContext("2d");
+
+//fill in code for canvas4 here
