89b095d9fe6f545ffb169279c357696369925e97
Jack <jackrojasduran25@email.com>
2026-06-16 11:40:58 -0400

    More describation/

--- projects/portfolio/script.js
+++ projects/portfolio/script.js
@@ -5,4 +5,25 @@ function showTab(tabId){
     tabs.forEach(tab=>{
         tab.classList.remove("active");
     });
+   document.getElementById(tabId)
+        .classList.add("active");
+}
+
+const intro =
+"Hi, I'm Jack Rojas Duran. Welcome to my portfolio.";
+
+let i = 0;
+
+function typeText(){
+
+    if(i < intro.length){
+
+        document.getElementById("typing")
+            .textContent += intro.charAt(i);
+
+        i++;
+
+        setTimeout(typeText,50);
+    }
+}
 
